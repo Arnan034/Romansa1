e5ca7e4893788f31cc8419a0d260b41790c0645f
Arnan034 <117829572+Arnan034@users.noreply.github.com>
2022-12-05 14:59:07 +0700

    Add files via upload

---
+++ Hitung Luas Persegi Panjang.cpp
@@ -0,0 +1,29 @@
+/* 
+ Deskripsi : Program Menentukan Luas Persegi Panjang.
+ I.S. : diketahui nilai panjang dan lebar sebuah persegi panjang dari pembaca program kemudian tentukan nilai luas persegi panjang.
+ F.S. : nilai luas persegi panjang telah ditemukan.
+ Nama/Author : Arnanda Prasatya
+ Tgl/Versi : 2 November 2022/ 01
+ */
+ 
+#include<stdio.h>
+// deklarasi modul
+float hitungLuasPersegiPanjang(float p, float l);
+
+int main(){
+	// Deklarasi main
+	float Panjang, Lebar, Luas;
+	// algoritma
+	scanf("%f %f", &Panjang, &Lebar);
+	Luas = hitungLuasPersegiPanjang(Panjang, Lebar);
+	printf("Luas Persegi Panjang adalah %f", Luas);
+	return 0;
+}
+
+float hitungLuasPersegiPanjang(float p, float l){
+	// deklarasi fungsi
+	float L;
+	// algoritma
+	L = p*l;
+	return L;
+}
